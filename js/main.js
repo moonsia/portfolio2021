ba98bea5757314a00b5fd6d8ee4ad2223f24b05b
moonsia <silverha10su@gmail.com>
2021-12-30 19:52:44 +0900

    12/30

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const hamburgerBtn = document.querySelector(".hamburger");
 const subMenu = document.querySelector("header>nav>ul");
 const snsIcon = document.querySelector(".instagram_icon");
+const runLeft = document.querySelector("svg text");
 
+console.log(runLeft);
 
 hamburgerBtn.addEventListener("click",function(){
     subMenu.classList.toggle("active");
     snsIcon.classList.toggle("active");
 })
+
+console.dir(runLeft);
+runLeft.addEventListener("mouseover", function(){
+    stopRun();
+    
+})
+
+function stopRun(){
+    clearInterval();
+}
+
+function textRun(){
+    runLeft
+}
