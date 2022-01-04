800c05cff78344062ee64db57e8090b387422558
moonsia <silverha10su@gmail.com>
2022-01-04 17:37:58 +0900

    0104'

--- js/main.js
+++ js/main.js
@@ -3,9 +3,25 @@ const subMenu = document.querySelector("header>nav>ul");
 const snsIcon = document.querySelector(".instagram_icon");
 const runLeft = document.querySelector("svg text");
 
-console.log(runLeft);
+let ovf,fortP
 
 hamburgerBtn.addEventListener("click",function(){
     subMenu.classList.toggle("active");
     snsIcon.classList.toggle("active");
 });
+
+
+$(function(){
+    ovf = this.getElementById("skill");
+    fortP = this.getElementById("portfolio");
+    winResize();
+    $(window).bind({resize: winResize, scroll: winScroll});
+    });
+    function winResize(){
+    ovf.style.top = fortP.offsetHeight + "px";
+    }
+    
+    function winScroll(){
+    var op = 1 - (window.pageYOffset / fortP.offsetHeight);
+    fortP.style.opacity = op;
+    }
