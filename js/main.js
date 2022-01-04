86a86a8f24c84af5d3c933cce6f26c2b1dd9d871
moonsia <silverha10su@gmail.com>
2022-01-04 20:11:12 +0900

    0104+

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const subMenu = document.querySelector("header>nav>ul");
 const snsIcon = document.querySelector(".instagram_icon");
 const runLeft = document.querySelector("svg text");
 
-let ovf,fortP
+
 
 hamburgerBtn.addEventListener("click",function(){
     subMenu.classList.toggle("active");
@@ -11,17 +11,3 @@ hamburgerBtn.addEventListener("click",function(){
 });
 
 
-$(function(){
-    ovf = this.getElementById("skill");
-    fortP = this.getElementById("portfolio");
-    winResize();
-    $(window).bind({resize: winResize, scroll: winScroll});
-    });
-    function winResize(){
-    ovf.style.top = fortP.offsetHeight + "px";
-    }
-    
-    function winScroll(){
-    var op = 1 - (window.pageYOffset / fortP.offsetHeight);
-    fortP.style.opacity = op;
-    }
