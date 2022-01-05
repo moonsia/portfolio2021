9fc9ce58f1def96832db8cff4b3cbd09c5fb0652
moonsia <silverha10su@gmail.com>
2022-01-05 17:00:55 +0900

    0105

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 const hamburgerBtn = document.querySelector(".hamburger");
 const subMenu = document.querySelector("header>nav>ul");
+const sublistA = document.querySelectorAll("header>nav>ul>li>a");
 const snsIcon = document.querySelector(".instagram_icon");
 const runLeft = document.querySelector("svg text");
 
+
+for(let i = 0; i<sublistA.length; i++){
+    sublistA[i].addEventListener("click",function(){
+        subMenu.classList.toggle("active");
+        snsIcon.classList.toggle("active");
+    });
+}
 
 
 hamburgerBtn.addEventListener("click",function(){
     subMenu.classList.toggle("active");
     snsIcon.classList.toggle("active");
+    
 });
-
 
